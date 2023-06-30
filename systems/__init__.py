a67454ead6fa161732e1af6315c5b139efa89312
MB-29 <55185203+MB-29@users.noreply.github.com>
2023-06-30 14:18:00 +0200

    rm static cartpole

--- systems/__init__.py
+++ systems/__init__.py
@@ -1,4 +1,3 @@
 from systems.dipole import Dipole
 from systems.arm import Arm
 from systems.cartpole import ActuatedCartpole, DampedActuatedCartpole
-from systems.static_cartpole import StaticCartpole
