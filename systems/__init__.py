3acd796937f65e734aacf922ffa9b58d0bcebd23
MB-29 <55185203+MB-29@users.noreply.github.com>
2023-08-16 11:36:10 +0200

    quadrupole

--- systems/__init__.py
+++ systems/__init__.py
@@ -1,4 +1,5 @@
 from systems.dipole import Dipole
+from systems.quadrupole import Quadrupole
 from systems.static_arm import Arm
 from systems.arm import ActuatedArm
-from systems.cartpole import ActuatedCartpole, DampedActuatedCartpole
+from systems.cartpole import ActuatedCartpole, DampedActuatedCartpole, Upkie
